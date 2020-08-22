6ec6263a87b7bebbc41937d933a49dbb7d69aca4
Someone-1243 <justinhye@gmail.com>
2020-08-21 23:21:42 -0400

    names

--- test/test.py
+++
@@ -1,5 +0,0 @@
-import diffbot
-
-json_result = diffbot.article('https://www.britannica.com/story/why-is-the-mona-lisa-so-famous', token='d656578220cbf622d16575aba331d47d')
-print(json_result['objects'][0]['text'])
-
