8b3fa79351b8dc23fa1b6b6ca69e737af1063d81
Someone-1243 <justinhye@gmail.com>
2020-08-22 11:03:41 -0400

    testing

--- scrape/pdf_test.py
+++ scrape/pdf_test.py
@@ -1,14 +1,14 @@
 import fitz
 import sys
 
-doc = fitz.open('demo.pdf')
+doc = fitz.open('2008.09077.pdf')
 pages = len(doc)
 
 fout = open('demo_out.txt', 'w', encoding='utf-8')
 
 for page in doc:
     text = page.getText()
-    # print(text)
+    print(text)
     fout.write(text)
 
 fout.close()
--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,6 +1,7 @@
 import diffbot
 
-json_result = diffbot.article('http://www.topsprogram.ca/reading-like-writers/', token='d656578220cbf622d16575aba331d47d')
+url = 'https://globalnews.ca/news/7291374/after-cerb-exhausted-ei-benefits/'
+json_result = diffbot.article(url, token='d656578220cbf622d16575aba331d47d')
 
 print(json_result['objects'][0]['text'])
 
