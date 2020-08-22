68d15accd8e4be86fe1ac4cf88f789410d9da90e
Someone-1243 <justinhye@gmail.com>
2020-08-22 11:14:07 -0400

    testing

--- scrape/scrape.py
+++
@@ -1,8 +0,0 @@
-import diffbot
-
-url = 'https://globalnews.ca/news/7291374/after-cerb-exhausted-ei-benefits/'
-json_result = diffbot.article(url, token='d656578220cbf622d16575aba331d47d')
-
-print(json_result['objects'][0]['text'])
-
-
