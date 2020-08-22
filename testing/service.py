991425506c5c71dd7ba9fdf680071649ea647618
Someone-1243 <justinhye@gmail.com>
2020-08-22 18:12:20 -0400

    word priorities

--- testing/service.py
+++ testing/service.py
@@ -4,6 +4,14 @@ import diffbot
 from nltk import tokenize
 import unicodedata
 
+def wordCount(dictionary, arr):
+    for elem in arr:
+        str = elem.lower()
+        if str in dictionary:
+            dictionary[str] += 1
+        else:
+            dictionary[str] = 1
+
 def clean(text):
     text = text.replace('\\n', "")
     text = unicodedata.normalize('NFKD', text)
@@ -69,11 +77,18 @@ def chunk(text):
 
     return final_result
 
-def get_keywords(url, dict):
+def bankKeys(dict, key):
+
+    str = key.lower()
+    if str in dict:
+        dict[str] += 1
+    else:
+        dict[str] = 1
+
+def get_keywords(url, dict, bank):
     subscription_key = "80ebba8cc9bf43fab4ef65f8891e8737"
     endpoint = "https://justin.cognitiveservices.azure.com/"
     keyphrase_url = endpoint + "/text/analytics/v3.0/keyphrases"
-    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
 
     # if url
     json_result = diffbot.article(url, token='d656578220cbf622d16575aba331d47d')
@@ -81,28 +96,43 @@ def get_keywords(url, dict):
     documents = chunk(clean(text))
 
     key_phrases = []
-    count = 1
     for batch in documents:
-        print(count)
-        count += 1
-
-        # empty lists should not be used
-        if not batch['documents']:
-            break
-
+        headers = {"Ocp-Apim-Subscription-Key": subscription_key}
         response = requests.post(keyphrase_url, headers=headers, json=batch)
-
         doc_list = response.json()['documents']
         for doc in doc_list:
             key_phrases += (doc['keyPhrases'])
+            for key in doc['keyPhrases']:
+                bankKeys(bank, key)
+                arr = key.split(' ')
+                wordCount(dict, arr)
+
+    print(key_phrases)
+    #print(dict)
 
     # if pdf --- needs to be implemented
 
+def sortOrder(dict):
+    sorted_x = sorted(dict.items(), key=lambda kv: kv[1], reverse=True)
+    return sorted_x
+
+
+def bestKeys(list, keys):
+    x = 0
 
 url = 'https://plato.stanford.edu/entries/medicine/'
 # urlNoNames = 'http://www.topsprogram.ca/all-the-worlds-a-stage/'
 urlTransfomer = 'https://jalammar.github.io/illustrated-transformer/'
 #
-# get_keywords('https://www.sciencedirect.com/science/article/pii/S0049384820301407')
-get_keywords('https://en.wikipedia.org/wiki/John_Oliver')
+dict = {}
+bank = {}
+get_keywords(url, dict, bank)
+sorted = sortOrder(dict)
+print(dict)
+print(sorted)
+print(bank)
+
+#url = 'https://en.wikipedia.org/wiki/John_Oliver'
+#get_keywords(url)
+
 
