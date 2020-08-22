23f5f63ee4c25dbafb5b92f0c08ee332793c6d55
Someone-1243 <justinhye@gmail.com>
2020-08-22 09:30:49 -0400

    rake

--- rakeTest/rakeTest.py
+++ rakeTest/rakeTest.py
@@ -1,8 +1,15 @@
 import operator
 from rake_nltk import Rake
+from rake_nltk import Metric
 import diffbot
 
-r = Rake()
+f = open('finalList.txt', 'r', encoding='utf-8')
+words = f.read()
+f.close()
+
+stop = words.split('\n')
+
+r = Rake(ranking_metric=Metric.WORD_FREQUENCY)
 
 url = 'https://www.cnn.com/2020/08/21/politics/peter-rafael-dzibinski-debbins-green-beret-russia/index.html'
 urlNoNames = 'https://www.britannica.com/science/influenza'
@@ -11,6 +18,8 @@ json_result = diffbot.article(urlNoNames, token='d656578220cbf622d16575aba331d47
 words = (json_result['objects'][0]['text'])
 r.extract_keywords_from_text(words)
 
-print(r.get_ranked_phrases_with_scores())
+result = r.get_ranked_phrases_with_scores()
+
+print(result)
 
 
